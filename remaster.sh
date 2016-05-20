57403d9bb1697aee6a4827a0fcfe7e724953813c
stanley karunditu <stanley@linuxsimba.com>
2016-05-19 21:41:16 -0400

    forked project then change user to vagrant and add acpid. Will use this ISO to build a packer file.. will suck it into the packer-libvirt-profiles project

--- remaster.sh
+++ remaster.sh
@@ -4,35 +4,35 @@ set -e
 
 # Remastering TinyCore for Packer Usage
 #
-# This script will create a TinyCore ISO that is ready to be used in Packer. It is 
+# This script will create a TinyCore ISO that is ready to be used in Packer. It is
 # an automated version of the guide at:
 #
 #   http://wiki.tinycorelinux.net/wiki:remastering
-# 
+#
 # You need to run this on a system that has following commands available:
 #   * unsquashfs (squashfs-tools)
-#   * advdef (advancecomp) 
+#   * advdef (advancecomp)
 #   * mkisofs (mkisofs)
 #
 # Additional customizations can be added to the customize function.
 #
 # Feel free to include additional extension by adding them to the following array:
 
-readonly EXTENSIONS=(bash openssh scsi-3.8.13-tinycore)
+readonly EXTENSIONS=(bash openssh acpid)
 
 # Global variables
-readonly MIRROR_URL=http://distro.ibiblio.org/tinycorelinux/5.x/x86
+readonly MIRROR_URL=http://distro.ibiblio.org/tinycorelinux/7.x/x86
 readonly DIST=./dist
 readonly BUILD=./build
 readonly DOWNLOADS=./downloads
 
 main() {
   prepare
-  explode_iso 
+  explode_iso
   download_extensions
   unpack_extensions
-  customize_packer
-  customize_scsi
+  customize_vagrant
+  customize_acpid
   repack_core
   remaster_iso
   calculate_checksum
@@ -54,7 +54,7 @@ download() {
   local file=$DOWNLOADS/${url##*/}
 
   if [[ ! -f "$file" ]]; then
-    wget -q -P $DOWNLOADS $url 
+    wget -q -P $DOWNLOADS $url
   fi
 }
 
@@ -82,7 +82,7 @@ explode_iso() {
   download $url
 
   [[ ! -d "$source" ]] && mkdir -p $source
-  
+
   mount $iso $source -o loop,ro
   cp -a $source/boot $DIST
   zcat $DIST/boot/core.gz | (cd $BUILD && cpio -i -H newc -d)
@@ -113,33 +113,54 @@ remaster_iso() {
   mv core.gz $DIST/boot
   mkisofs -l -J -R -V TC-custom -no-emul-boot -boot-load-size 4 \
     -boot-info-table -b boot/isolinux/isolinux.bin \
-    -c boot/isolinux/boot.cat -o tinycore-packer.iso $DIST
+    -c boot/isolinux/boot.cat -o tinycore-vagrant.iso $DIST
 }
 
 calculate_checksum() {
-  local md5=($(md5sum tinycore-packer.iso)) 
+  local md5=($(md5sum tinycore-vagrant.iso))
 
   echo "Remastering done. The md5 checksum of the new iso is: ${md5}"
 }
 
-customize_packer() {
+customize_vagrant() {
   ( cd $BUILD/usr/local/etc/ssh \
       && mv sshd_config.example sshd_config \
       && sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' sshd_config
   )
-  
-  echo "packer:x:1002:50::/home/packer:/bin/sh" >> $BUILD/etc/passwd
-  echo 'packer:$6$zHqBItBy$ysrNi5D/JwJNEsVI/5eA305vsA0GsWyT4.0K3AAL.R28TxioZRZD9yplyV5FwrIs0FJc5kS0M0/HHo2N4FBUk1:16240:0:999999:7:::' >> $BUILD/etc/shadow
-  mkdir -p $BUILD/home/packer
-  chown 1002:50 $BUILD/home/packer
-  echo "packer	ALL=NOPASSWD: ALL" >> $BUILD/etc/sudoers
+
+  echo "vagrant:x:1002:50::/home/vagrant:/bin/sh" >> $BUILD/etc/passwd
+  echo 'vagrant:$6$zaJNfMJj$X/Rz7jgVcgYc7pP6UpfjZxGE1xgFIF2etlIAKSITix7F8u4zBvGazVW/Y25/EbbJzlqFCBAzh/nL2tNyBqA7i.:16240:0:999999:7:::' >> $BUILD/etc/shadow
+  mkdir -p $BUILD/home/vagrant
+  chown 1002:50 $BUILD/home/vagrant
+  echo "vagrant	ALL=NOPASSWD: ALL" >> $BUILD/etc/sudoers
   sed -i 's/tty1::respawn:\/sbin\/getty -nl \/sbin\/autologin 38400 tty1/tty1::respawn:\/sbin\/getty 38400 tty1/' $BUILD/etc/inittab
-  echo "/usr/local/etc/init.d/openssh start" >> $BUILD/opt/bootlocal.sh 
+  echo "/usr/local/etc/init.d/openssh start" >> $BUILD/opt/bootlocal.sh
 }
 
-customize_scsi() {
-  echo "depmod -a" >> $BUILD/opt/bootlocal.sh 
-  echo "modprobe mptspi" >> $BUILD/opt/bootlocal.sh 
+customize_acpid() {
+  mkdir -p $BUILD/usr/local/etc/acpi/events/
+  mkdir -p $BUILD/home/tc/.acpi/
+  echo "event=.*" >> $BUILD/usr/local/etc/acpi/events/all
+  echo 'action=/home/tc/.acpi/gen.sh "%e"' >> $BUILD/usr/local/etc/acpi/events/all
+  cat <<'EOF' > $BUILD/home/tc/.acpi/gen.sh
+#!/bin/sh
+case $1 in
+    button/power*)
+       exitcheck.sh shutdown;; #power button code, with backup
+    button/sleep*)
+        echo -n "mem" > /sys/power/state;; # sleep button code
+    "hotkey ATKD 0000001a"*) # special key #1 extra assigned sleep button
+	 echo -n "mem" > /sys/power/state;;
+    "hotkey ATKD 0000001d"*)
+         exitcheck.sh reboot;; # special key #4 reboot with backup
+    # *)
+    # popup $1;;
+esac
+EOF
+  chown -R 1001 $BUILD/home/tc/.acpi
+  chmod 755 $BUILD/home/tc/.acpi/gen.sh
+
+  echo "acpid" >> $BUILD/opt/bootlocal.sh
 }
 
 main
